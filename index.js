9fc2f049c847190314a2a5ea4df4e69940cc26d6
anukritigoyal <anukriti@uw.edu>
2019-05-02 20:46:04 -0700

    completed viz

--- index.js
+++ index.js
@@ -9,10 +9,10 @@
   window.onload = function() {
     svgContainer = d3.select('body')
       .append('svg')
-      .attr('width', 500)
-      .attr('height', 500);
+      .attr('width', 700)
+      .attr('height', 700);
     // d3.csv is basically fetch but it can be be passed a csv file as a parameter
-    d3.csv("data.csv")
+    d3.csv("dataEveryYear.csv")
       .then((data) => makeScatterPlot(data));
   }
 
@@ -20,12 +20,11 @@
   function makeScatterPlot(csvData) {
     data = csvData // assign data as global variable
 
-    // get arrays of fertility rate data and life Expectancy data
-    let fertility_rate_data = data.map((row) => parseFloat(row["fertility_rate"]));
+    let fertility_data = data.map((row) => parseFloat(row["fertility_rate"]));
     let life_expectancy_data = data.map((row) => parseFloat(row["life_expectancy"]));
 
     // find data limits
-    let axesLimits = findMinMax(fertility_rate_data, life_expectancy_data);
+    let axesLimits = findMinMax(fertility_data, life_expectancy_data);
 
     // draw axes and return scaling + mapping functions
     let mapFunctions = drawAxes(axesLimits, "fertility_rate", "life_expectancy");
@@ -40,19 +39,19 @@
   // make title and axes labels
   function makeLabels() {
     svgContainer.append('text')
-      .attr('x', 100)
+      .attr('x', 250)
       .attr('y', 40)
       .style('font-size', '14pt')
       .text("Countries by Life Expectancy and Fertility Rate");
 
     svgContainer.append('text')
-      .attr('x', 130)
-      .attr('y', 490)
+      .attr('x', 230)
+      .attr('y', 690)
       .style('font-size', '10pt')
       .text('Fertility Rates (Avg Children per Woman)');
 
     svgContainer.append('text')
-      .attr('transform', 'translate(15, 300)rotate(-90)')
+      .attr('transform', 'translate(15, 400)rotate(-90)')
       .style('font-size', '10pt')
       .text('Life Expectancy (years)');
   }
@@ -67,11 +66,12 @@
     let pop_map_func = d3.scaleLinear()
       .domain([pop_limits[0], pop_limits[1]])
       .range([3, 20]);
-
+    //get years for drop down
+    let years = getFilters(data);
     // mapping functions
     let xMap = map.x;
     let yMap = map.y;
-
+   
     // make tooltip
     let div = d3.select("body").append("div")
     .attr("class", "tooltip")
@@ -81,51 +81,51 @@
 
     // append data to SVG and plot as points
     let circles = svgContainer.selectAll('.dot')
-      .data(data)
+      .data(data.filter(function(d){return d.time==years[0]}))
       .enter()
       .append('circle')
         .attr('cx', xMap)
         .attr('cy', yMap)
-        .attr('r', (d) => pop_map_func(d["pop_mlns"]))
+        .attr('r', (d) => pop_map_func(d["pop_mlns"])*1.5)
         .attr('fill', "#4286f4")
-        // .on("mouseout", (d) => {
-        // circles.transition()
-        //   .attr('cx', xMap)
-        //   .attr('cy', yMap)
-        //   .attr('r', (d) => pop_map_func(d["pop_mlns"]))
-        //   div.transition()
-        //   .duration(500)
-        //   .style("opacity", 0);
-        
-        // });
+        // add tooltip functionality to points
+        .on("mouseover", (d) => {
+          div.transition()
+            .duration(200)
+            .style("opacity", .9);
+          div.html(d.location + "<br/>" + "population: "+ numberWithCommas(d["pop_mlns"]*1000000) + "<br/>" + 
+          "fertility rate: " + d.fertility_rate + "<br/>" + "life expectancy: " + d.life_expectancy)
+            .style("left", (d3.event.pageX + 20) + "px")
+            .style("top", (d3.event.pageY) + "px");
+        })
+        .on("mouseout", (d) => {
+          div.transition()
+            .duration(500)
+            .style("opacity", 0);
+        });
 
-    // circles.on("mouseover", (d) => {
-    //   circles
-    //     .transition()
-    //       .attr('cx', xMap((d)))
-    //       .attr('cy', yMap(d))
-    //       .attr("fill", "black")
-    //       .attr('r', pop_map_func(d["pop_mlns"]))
-    //       .duration(2500);
-    });
+        //create dropdown
 
-    var dropDown = d3.select('body')
-      .append('select')
-      .on('change', function() {
-        var selected = this.value;
-        var thisObject = data.filter(country => country.location == selected)[0];
-        circles.transition()
-          .attr('cx', xMap(thisObject))
-          .attr('cy', yMap(thisObject))
-          .attr('r', pop_map_func(thisObject['pop_mlns']))
-          .duration(2000);
+      var dropDown = d3.select('body')
+        .append('select')
+        .on('change', function() {
+            var selected = this.value;
+            var thisObject = data.filter(time => time.time == selected);
+            circles.data(thisObject)
+                .transition()
+                  .attr('cx', xMap)
+                  .attr('cy', yMap)
+                  .attr('r', (d) => pop_map_func(d["pop_mlns"]))
+                  .attr('fill', "#4286f4")
       });
 
     var options = dropDown.selectAll('option')
-      .data(data)
+      .data(years)
       .enter()
         .append('option')
-        .text((d) => { return d.location; });
+        .text((d) => { return d; });
+
+        
   }
 
   // draw the axes and ticks
@@ -136,7 +136,7 @@
     // function to scale x value
     let xScale = d3.scaleLinear()
       .domain([limits.xMin - 0.5, limits.xMax + 0.5]) // give domain buffer room
-      .range([50, 450]);
+      .range([50, 650]);
 
     // xMap returns a scaled x value from a row of data
     let xMap = function(d) { return xScale(xValue(d)); };
@@ -144,7 +144,7 @@
     // plot x-axis at bottom of SVG
     let xAxis = d3.axisBottom().scale(xScale);
     svgContainer.append("g")
-      .attr('transform', 'translate(0, 450)')
+      .attr('transform', 'translate(0, 650)')
       .call(xAxis);
 
     // return y value from a row of data
@@ -153,7 +153,7 @@
     // function to scale y
     let yScale = d3.scaleLinear()
       .domain([limits.yMax + 5, limits.yMin - 5]) // give domain buffer
-      .range([50, 450]);
+      .range([50, 650]);
 
     // yMap returns a scaled y value from a row of data
     let yMap = function (d) { return yScale(yValue(d)); };
@@ -192,7 +192,11 @@
       yMax : yMax
     }
   }
-
+  function getFilters(data){
+      let years = data.map((row) => parseFloat(row["time"]));
+      years = [... new Set(years)];
+      return years;
+  }
   // format numbers
   function numberWithCommas(x) {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
